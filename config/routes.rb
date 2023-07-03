ff9529fbab142a0d2abdac56f5408571fd438faf
kusakabenanami <138275920+kusakabenanami@users.noreply.github.com>
2023-07-03 03:57:47 +0000

    [Finish]3-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  get 'lists/show'
+  # .../lists/1 や .../lists/3 に該当する
+  get 'lists/:id' => 'lists#show'
   get 'lists/edit'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # .../lists/1 や .../lists/3 に該当する
+  get 'lists/:id' => 'lists#show', as: 'list'
 end
