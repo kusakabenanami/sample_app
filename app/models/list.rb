a224aa408c39a6a6a759158ac8f56ab63a9b440f
kusakabenanami <138275920+kusakabenanami@users.noreply.github.com>
2023-07-03 09:06:34 +0000

    [Finish]3-8

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,7 @@
 class List < ApplicationRecord
   has_one_attached :image
+
+  validates :title, presence: true #trueはデータが存在しなければならないという設定
+  validates :body, presence: true
+  validates :image, presence: true
 end
