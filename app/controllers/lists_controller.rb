77ecb662c0c0b3fd3fce5001d5f0a4a3cc615c51
kusakabenanami <138275920+kusakabenanami@users.noreply.github.com>
2023-07-03 08:33:09 +0000

    [Finish]3-7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,9 +7,7 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    # redirect_to '/top' を削除して、以下コードに変更
-    # 詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+    redirect_to list_path(list.id)# 詳細画面へリダイレクト
   end
 
   def index
@@ -28,6 +26,12 @@ class ListsController < ApplicationController
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
   end
 
   private
